8906b0794f0f65a7ca629586ea9516248832b946
Martin Tiscordio <martisco3533@hotmail.com>
2022-07-08 01:13:07 -0300

    Finish 7/7

--- src/main.js
+++ src/main.js
@@ -8,13 +8,12 @@ const api = axios.create({
     },
 });
 
-async function getTrendingMoviesPreview() {
-    const { data } = await api("trending/movie/day");
-    const movies = data.results;
+//Utils
+
+function createMovies(movies, container) {
+    container.innerHTML = "";
 
     movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList")
-
         const movieContainer = document.createElement("div");
         movieContainer.classList.add("movie-container");
         
@@ -24,31 +23,56 @@ async function getTrendingMoviesPreview() {
         movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300" + movie.poster_path,);
 
         movieContainer.appendChild(movieImg);
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
+        container.appendChild(movieContainer);
     });
 }
 
-async function getCategoriesPreview() {
-    const { data } = await api("genre/movie/list");
-    const categories = data.genres;
+function createCategories(categories, container) {
+    container.innerHTML = "";
 
     categories.forEach(category => {
-        const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list")
-
         const categoryContainer = document.createElement("div");
         categoryContainer.classList.add("category-container");
         
         const categoryTitle = document.createElement("h3");
         categoryTitle.classList.add("category-title");
         categoryTitle.setAttribute("id", "id" + category.id);
+        categoryTitle.addEventListener("click", () => {
+            location.hash =`#category=${category.id}-${category.name}`;
+        });
         const categoryTitleText = document.createTextNode(category.name);
 
         categoryTitle.appendChild(categoryTitleText);
         categoryContainer.appendChild(categoryTitle);
-        previewCategoriesContainer.appendChild(categoryContainer);
-
+        container.appendChild(categoryContainer);
     });
 }
 
-getTrendingMoviesPreview();
-getCategoriesPreview();
+//Llamados a la API
+
+
+async function getTrendingMoviesPreview() {
+    const { data } = await api("trending/movie/day");
+    const movies = data.results;
+
+    createMovies(movies, trendingMoviesPreviewList);
+
+}
+
+async function getCategoriesPreview() {
+    const { data } = await api("genre/movie/list");
+    const categories = data.genres;
+
+    createCategories(categories, categoriesPreviewList);
+}
+
+async function getMoviesByCategory(id) {
+    const { data } = await api("discover/movie", {
+        params: {
+            with_genres: id,
+        }
+    });
+    const movies = data.results;
+
+    createMovies(movies, genericSection);
+}
